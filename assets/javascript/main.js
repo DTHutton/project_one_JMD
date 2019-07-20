54d416b8044373a799aee2e33d3007b945aa55e3
Mnomens <48028258+Mnomens@users.noreply.github.com>
2019-07-20 12:34:55 -0400

    added character generation, still needs work

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,37 @@
-$(document).ready(function () {
 
+let characters = [];
+//this fills our characters array so we have 10 characters
+function generateCharacters() {
 
-    let characters = [];
-
-    let generateCharacters = function() {
-
-        for(let i = 0; i<10; i++) {
-            
-        }
+    for(let i = 0; i<10; i++) {
+        characters.push( {
+            name: "Character" + i,
+            hp: 50,
+            attack: 5
+        });
     }
+    console.log(characters);
+}
    
 
 
+function generateCharacterButtons() {
+    
+    for(let j = 0; j < 10; j++) {
+        let createCharacters = `
+        <h1>${characters[j]}</h1>
+        `
+        $("").append(createCharacters);
+    }
+    
+}
+generateCharacters();
+generateCharacterButtons();
 
 
 
 
-});
+
+
+
+
