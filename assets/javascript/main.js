4c30483d944d182f6d37d384141a4f5ff788bc70
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-07-25 21:24:21 -0400

    started on css

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,37 +1,5 @@
 $(document).ready(function () {
 
-
-    let characters = [];
-    //this fills our characters array so we have 10 characters
-    function generateCharacters() {
-
-        for (let i = 0; i < 10; i++) {
-            characters.push({
-                name: "Character" + i,
-                hp: 50,
-                attack: 5
-            });
-        }
-        console.log(characters);
-    }
-
-
-
-    function generateCharacterButtons() {
-
-        for (let j = 0; j < 10; j++) {
-            let createCharacters = `
-        <h1>${characters[j]}</h1>
-        `
-            $("").append(createCharacters);
-        }
-
-    }
-    generateCharacters();
-    generateCharacterButtons();
-
-
-
     //character portrait values generator
     const charObj = {
         sassyChicken: "ftqLysT45BJMagKFuk",
@@ -63,28 +31,27 @@ $(document).ready(function () {
             type: "GET",
             url: giphyQuery,
             success: function (response) {
+                console.log(response);
 
                 let results = response.data;
                 let stillUrl = results.images.fixed_height_still.url;
                 let animateUrl = results.images.fixed_height.url;
 
                 let charTemplate = `
-            <div class="card m-1" style="width: 18rem;" data-id="${btnThing}">
-                <img src="${stillUrl}" class="card-img-top portrait" data-animate="${animateUrl}" data-still="${stillUrl}" data-state="still" alt="placeholder">
+            <div class="card m-1 border border-dark" style="width: 18rem; height: 24rem;" data-id="${btnThing}">
+                <img src="${stillUrl}" class="card-img-top portrait border border-dark" data-animate="${animateUrl}" data-still="${stillUrl}" data-state="still" alt="placeholder">
                 <div class="card-body">
-                    <h5 class="card-title">Character Name: ${charName}</h5>
+                    <h5 class="card-title text-center py-1">${charName}</h5>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">HP: ${hpVal}</li>
-                    <li class="list-group-item">Attack: ${attVal}</li>
+                <ul class="list-group list-group-flush ">
+                    <li class="list-group-item text-center py-1">HP: ${hpVal} Attack: ${attVal}</li>
                 </ul>
                 <div class="card-body">
-                    <a href="#" class="card-link" id="attack" data-id="${btnThing}">Attack Button</a>
-                    <a href="#" class="card-link" id="taunt" data-id="${btnThing}">Taunt Button</a>
+                    <a href="#" class="card-link py-1" id="attack" data-id="${btnThing}">Attack Button</a>
+                    <a href="#" class="card-link py-1" id="taunt" data-id="${btnThing}">Taunt Button</a>
                 </div>
             </div>
             `
-
                 $("#characterPortrait").append(charTemplate);
             }
         });
