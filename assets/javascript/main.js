5e5c147cf72f42fdfd96c8ac76f8722f054b1e84
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-07-30 16:13:27 -0400

    updated main.js with stylings in template strings.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -45,17 +45,19 @@ $(document).ready(function () {
                     let animateUrl = results.images.fixed_height.url;
 
                     let charTemplate = `
-                            <div class="card m-1" id="${characters[k].name}" style="width: 18rem;" data-attack-power="${characters[k].attack}" data-health-power="${characters[k].health}">
+                        <section class="col-3">
+                            <div class="card m-1 border border-dark" id="${characters[k].name}" style="width: 18rem;" data-attack-power="${characters[k].attack}" data-health-power="${characters[k].health}">
                                 <img src="${stillUrl}" class="card-img-top portrait" data-animate="${animateUrl}" data-still="${stillUrl}" data-id="${characters[k].name}" 
                                     data-state="still" alt="placeholder">
                                 <div class="card-body">
-                                    <h5 class="card-title">${characters[k].name}</h5>
+                                    <h5 class="card-title text-center character-font">${characters[k].name}</h5>
                                 </div>
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item" id="show-health">HP: ${characters[k].health}</li>
-                                    <li class="list-group-item" id="show-attack">Attack: ${characters[k].attack}</li>
+                                    <li class="list-group-item character-font" id="show-health">HP: ${characters[k].health}</li>
+                                    <li class="list-group-item character-font" id="show-attack">Attack: ${characters[k].attack}</li>
                                 </ul>
                             </div>
+                        </section>    
                             `
 
                     $("#characterPortrait").append(charTemplate);
